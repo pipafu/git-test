04809d4b48bb0d34772a72638652dea9cb06255f
pipafu <764711900@qq.com>
2021-12-03 22:33:05 +0800

    hot2-commit

--- src/main/java/com/fu/git/GitTest.java
+++ src/main/java/com/fu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello54321");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 
 }
