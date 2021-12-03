2c5ee0e9e198d8df3ddb997d8ecad48c0b83ceaa
pipafu <764711900@qq.com>
2021-12-03 22:53:50 +0800

    push commit1

--- src/main/java/com/fu/git/GitTest.java
+++ src/main/java/com/fu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix2");
         System.out.println("hot-fix3");
+        System.out.println("push");
     }
 
 }
