721c6692455f4ef1c3531ac7f6ac64bcad10e59c
pipafu <764711900@qq.com>
2021-12-03 22:12:31 +0800

    first-commit

---
+++ src/main/java/com/fu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.fu.git;
+
+/**
+ * @author pipafu
+ * @create 2021/12/3
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
